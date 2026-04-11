3acd1ef3e1a6e4306a668555c5eccc247a952a0a
Stanislav Stoychev <stanislavstoychev98@gmail.com>
2026-04-11 20:17:19 +0300

    Add Author entity model and add AuthorID foreign key

---
+++ BookReviewHub/BookReviewHub/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookReviewHub.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(1000)]
+        public string? Biography { get; set; }
+
+        [StringLength(100)]
+        public string? Nationality { get; set; }
+
+        public int? BirthYear { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
--- BookReviewHub/BookReviewHub/Models/Book.cs
+++ BookReviewHub/BookReviewHub/Models/Book.cs
@@ -8,28 +8,29 @@ namespace BookReviewHub.Models
         public int Id { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [StringLength(200, MinimumLength = 1)]
         public string Title { get; set; } = null!;
 
-        [Required]
-        [StringLength(100)]
-        public string Author { get; set; } = null!;
-
-        [Required]
         [StringLength(2000)]
         public string? Description { get; set; }
 
-        [Range(0, 2026)]
+        [Range(1000, 2100)]
         public int PublicationYear { get; set; }
 
-        [Range(1, 5)]
-        public double Rating { get; set; }
+        [StringLength(255)]
+        public string? CoverImageUrl { get; set; }
 
         [Required]
         public int GenreId { get; set; }
 
         [ForeignKey(nameof(GenreId))]
         public Genre? Genre { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [ForeignKey(nameof(AuthorId))]
+        public Author? Author { get; set; }
 
         public ICollection<Review> Reviews { get; set; } = new List<Review>();
     }
