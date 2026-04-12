2baefd4919d4b1a339969ad7ae326d9825f8c9d0
Stanislav Stoychev <stanislavstoychev98@gmail.com>
2026-04-13 00:29:33 +0300

    Add application user model extending identity user

---
+++ BookReviewHub/BookReviewHub/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookReviewHub.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public string DisplayName { get; set; } = string.Empty;
+
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    }
+}
