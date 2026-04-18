0bc2a72666c08bcf1cb0ff8b366c6016f91c1ca6
Stanislav Stoychev <stanislavstoychev98@gmail.com>
2026-04-18 23:34:03 +0300

    add admin userscontroller and views

---
+++ BookReviewHub/BookReviewHub/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,84 @@
+﻿using BookReviewHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookReviewHub.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRoles = new Dictionary<string, IList<string>>();
+
+            foreach (var user in users)
+                userRoles[user.Id] = await _userManager.GetRolesAsync(user);
+
+            ViewBag.UserRoles = userRoles;
+            return View(users);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Promote(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            if (!await _userManager.IsInRoleAsync(user, "Administrator"))
+                await _userManager.AddToRoleAsync(user, "Administrator");
+
+            TempData["Success"] = $"{user.DisplayName} promoted to Administrator.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Demote(string userId)
+        {
+            var currentUserId = _userManager.GetUserId(User);
+            if (userId == currentUserId)
+            {
+                TempData["Error"] = "You cannot demote yourself.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            await _userManager.RemoveFromRoleAsync(user, "Administrator");
+            TempData["Success"] = $"{user.DisplayName} demoted to User.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(string userId)
+        {
+            var currentUserId = _userManager.GetUserId(User);
+            if (userId == currentUserId)
+            {
+                TempData["Error"] = "You cannot delete your own account.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            await _userManager.DeleteAsync(user);
+            TempData["Success"] = "User deleted.";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
