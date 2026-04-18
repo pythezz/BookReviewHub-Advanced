1e735c97a888390a5b76b89e32a04a195f6c5db3
Stanislav Stoychev <stanislavstoychev98@gmail.com>
2026-04-18 22:11:35 +0300

    scaffold admin area with views and route

--- BookReviewHub/BookReviewHub/Program.cs
+++ BookReviewHub/BookReviewHub/Program.cs
@@ -53,6 +53,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Books}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
